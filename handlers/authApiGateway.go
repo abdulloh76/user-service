17ecd34e592db7578ed0de50b26072f8e7444c25
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-10-05 16:47:29 +0500

    implement auth controller for awsApiGateway

---
+++ handlers/authApiGateway.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/abdulloh76/user-service/domain"
+	"github.com/abdulloh76/user-service/utils"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type AuthApiHandler struct {
+	auth *domain.AuthDomain
+}
+
+func NewAuthApiHandler(d *domain.AuthDomain) *AuthApiHandler {
+	return &AuthApiHandler{auth: d}
+}
+
+func (g *AuthApiHandler) SignUp(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if strings.TrimSpace(event.Body) == "" {
+		return utils.ErrResponse(http.StatusBadRequest, "empty request body"), nil
+	}
+
+	id, err := g.auth.CreateUser(ctx, []byte(event.Body))
+	if err != nil {
+		if errors.Is(err, utils.ErrJsonUnmarshal) {
+			return utils.ErrResponse(http.StatusBadRequest, err.Error()), nil
+		}
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+
+	return utils.Response(http.StatusOK, map[string]string{"id": id}), nil
+}
+
+func (g *AuthApiHandler) SignIn(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if strings.TrimSpace(event.Body) == "" {
+		return utils.ErrResponse(http.StatusBadRequest, "empty request body"), nil
+	}
+
+	token, err := g.auth.GenerateToken(ctx, []byte(event.Body))
+	if err != nil {
+		if errors.Is(err, utils.ErrWithDB) {
+			return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
+		}
+		return utils.ErrResponse(http.StatusBadRequest, err.Error()), nil
+	}
+
+	return utils.Response(http.StatusOK, map[string]string{"token": token}), nil
+}
+
+func (g *AuthApiHandler) AuthMiddleware(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if strings.TrimSpace(event.Body) == "" {
+		return utils.ErrResponse(http.StatusBadRequest, "empty request body"), nil
+	}
+
+	token, err := g.auth.ParseToken([]byte(event.Body))
+	if err != nil {
+		if errors.Is(err, utils.ErrInvalidJWTMethod) || errors.Is(err, utils.ErrInvalidTokenClaims) {
+			return utils.ErrResponse(http.StatusBadRequest, err.Error()), nil
+		}
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+
+	return utils.Response(http.StatusOK, map[string]string{"token": token}), nil
+}
---
+++ utils/awsGatewayResponse.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func Response(code int, object interface{}) events.APIGatewayProxyResponse {
+	marshalled, err := json.Marshal(object)
+	if err != nil {
+		return ErrResponse(http.StatusInternalServerError, err.Error())
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: code,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body:            string(marshalled),
+		IsBase64Encoded: false,
+	}
+}
+
+func ErrResponse(status int, body string) events.APIGatewayProxyResponse {
+	message := map[string]string{
+		"message": body,
+	}
+
+	messageBytes, _ := json.Marshal(&message)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(messageBytes),
+	}
+}
