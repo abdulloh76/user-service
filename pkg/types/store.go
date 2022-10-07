c2d80b90a1814b3a6f7ae929a889ae7f2bb04ffd
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-10-07 16:06:06 +0500

    refactor naming, custom errors

--- pkg/types/store.go
+++ pkg/types/store.go
@@ -3,11 +3,11 @@ package types
 import "context"
 
 type UserStore interface {
-	All(ctx context.Context) ([]User, error)
-	Get(ctx context.Context, id string) (*User, error)
-	Create(ctx context.Context, user UserBody) error
-	Update(ctx context.Context, id string, user UserBody) (*User, error)
-	Delete(ctx context.Context, id string) error
+	AllUsers(ctx context.Context) ([]User, error)
+	GetUserDetails(ctx context.Context, id string) (*User, error)
+	CreateUser(ctx context.Context, user UserBody) error
+	UpdateUserDetails(ctx context.Context, id string, user UserBody) (*User, error)
+	DeleteUser(ctx context.Context, id string) error
 }
 
 type AuthStore interface {
