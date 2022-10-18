c46ac14b5dbf71177989f94521e4eb3debf8fbbb
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-10-18 16:19:51 +0500

    new dto structs for user rest service

--- pkg/types/userDto.go
+++ pkg/types/userDto.go
@@ -6,3 +6,18 @@ type UserBody struct {
 	Email     string       `dynamodbav:"email" json:"email"`
 	Address   AddressModel `dynamodbav:"address" json:"address"`
 }
+
+type UpdateCredentialsDto struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+}
+
+type UpdateAddressDto struct {
+	Address AddressModel `json:"address"`
+}
+
+type UpdatePasswordDto struct {
+	CurrentPassword string `json:"currentPassword"`
+	NewPassword     string `json:"newPassword"`
+}
