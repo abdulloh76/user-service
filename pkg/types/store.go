e71492e77c2c4c28527a5bd980fd81c7e0a21afc
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-10-18 16:20:27 +0500

    update store with new db handlers

--- pkg/types/store.go
+++ pkg/types/store.go
@@ -3,14 +3,14 @@ package types
 import "context"
 
 type UserStore interface {
-	AllUsers(ctx context.Context) ([]User, error)
 	GetUserDetails(ctx context.Context, id string) (*User, error)
-	CreateUser(ctx context.Context, user UserBody) error
-	UpdateUserDetails(ctx context.Context, id string, user UserBody) (*User, error)
+	UpdateUserCredentials(ctx context.Context, id string, credentials UpdateCredentialsDto) error
+	UpdatePassword(ctx context.Context, id string, password string) error
+	UpdateAddress(ctx context.Context, id string, user AddressModel) error
 	DeleteUser(ctx context.Context, id string) error
 }
 
 type AuthStore interface {
-	CreateUser(ctx context.Context, userCredentials UserCredentials) (string, error)
+	CreateUser(ctx context.Context, userCredentials SignInCredentials) (string, error)
 	GetUser(ctx context.Context, email, passwordHash string) (*User, error)
 }
